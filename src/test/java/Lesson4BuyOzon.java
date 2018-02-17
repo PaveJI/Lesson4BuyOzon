a8a11c30553e9ffb0e41b86dadae0cf555796d6d
Pavel <pkipriyanov@tester.ru>
2018-02-17 18:36:45 +0500

    17022018

--- src/test/java/Lesson4BuyOzon.java
+++ src/test/java/Lesson4BuyOzon.java
@@ -35,15 +35,50 @@ public class Lesson4BuyOzon extends TestCase{
             searchButtonBooks2.click();
             //Thread.sleep(2000); // Let the user actually see something!
         }
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript("window.scrollBy(0,750)", "");
         //Thread.sleep(2000); // Let the user actually see something!
         //driver.quit();*/
-            WebElement searchBestsellers = driver.findElement(By.xpath("//*[@id=\"PageCenter\"]/div[6]/div/div[2]/div/div[2]/div/div[1]/a/div[1]/div/img"));
-            searchBestsellers.click();
+        WebElement searchBestsellers = driver.findElement(By.xpath("//*[@id=\"PageCenter\"]/div[6]/div/div[2]/div/div[2]/div/div[1]/a/div[1]/div/img"));
+        searchBestsellers.click();
+        WebElement addBasket = driver.findElement(By.xpath("//*[@id=\"PageCenter\"]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[5]/div"));
+        addBasket.click();
+        WebElement countBasket = driver.findElement(By.xpath("//*[@id=\"PageCenter\"]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[5]/div/a/span[2]"));
+        WebElement addOne = driver.findElement(By.xpath("//*[@id=\"PageCenter\"]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[5]/div/div"));
+        addOne.click();
+        //WebElement countBasket1 = driver.findElement(By.cssSelector("div#PageCenter span.eSaleBlockButton_label"));
+        WebElement wayToBasket = driver.findElement(By.xpath("//*[@id=\"PageCenter\"]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[5]/div"));
+        //wayToBasket.click();  работает через раз
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement element = driver.findElement(By.className("eSaleBlockButton_label"));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
 
-    }
+        WebElement element1 = driver.findElement(By.className("jsMinus"));
+        Thread.sleep(5000); // Let the user actually see something! // так делать плохо, но это работает
+        //WebElement dynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.className("jsMinus"))); //не работает
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // почему то не работает
+        JavascriptExecutor executor1 = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element1);
+        Thread.sleep(5000); // Let the user actually see something! // так делать плохо, но это работает
+        //executor.executeScript("arguments[0].click();", dynamicElement);
 
+        WebElement element2 = driver.findElement(By.className("jsMinus"));
+        JavascriptExecutor executor2 = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element2);
+        Thread.sleep(5000); // Let the user actually see something! // так делать плохо, но это работает
+
+        WebElement basketIsEmpty = driver.findElement(By.xpath("//*[@id=\"PageCenter\"]/div[2]/div[3]/div[1]/div[1]/div[1]/div/span"));
+        String empty = basketIsEmpty.getText();
+        System.out.println(empty + ", драйвер закрылся. Тест выполнен.");
+        driver.quit();
+
+
+        }
 }
+
+
+
+
 
 
